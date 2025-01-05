4d5480405c6c822c1558e69cb3f300841b78314f
Exulenood <sestak-p@outlook.com>
2025-01-05 19:16:49 +0100

    finish dynamic arrays

--- 02-memory-dive/dynamic_array.zig
+++ 02-memory-dive/dynamic_array.zig
@@ -10,13 +10,46 @@ pub fn main() !void {
     }
     const allocator = gpa.allocator();
 
-    var list = std.ArrayList(i32).init(allocator);
+    var list = try std.ArrayList(i32).initCapacity(allocator, 5);
     defer list.deinit();
 
-    try list.append(42);
-    try list.append(43);
-    try list.append(44);
+    try term_out.print("\n\nFilling array:\n\n", .{});
 
-    try term_out.print("Our Dynamic Array: {any}\n", .{list.items});
-    try term_out.print("Länge: {}\n", .{list.items.len});
+    var i: i32 = 10;
+    while (i < 20) : (i += 1) {
+        try list.append(i);
+        try term_out.print("After append with {}:\nlenght={}\ncapacity={}\n", .{ i, list.items.len, list.capacity });
+    }
+
+    try term_out.print("\n\nIteration Operations:\n\n1. by Index:\n", .{});
+    for (list.items, 0..) |value, index| {
+        try term_out.print("Value for Index {} = {}\n", .{ index, value });
+    }
+
+    try term_out.print("\n\n2. by Values:\n", .{});
+    for (list.items) |value| {
+        try term_out.print("Value {}\n", .{value});
+    }
+
+    try term_out.print("\n\n3. mutating (values * 2):\n", .{});
+    for (list.items) |*value| {
+        value.* *= 2;
+    }
+    try term_out.print("List after mutating iteration{any}\n", .{list.items});
+
+    try term_out.print("\n\nRemove Operations:\n\n", .{});
+
+    _ = list.orderedRemove(1);
+    try term_out.print("After an ordered remove, value on index 1 is taken out and all following values moved in by 1 index: {any}\n", .{list.items});
+
+    _ = list.swapRemove(0);
+    try term_out.print("After a swap remove, value on index 0 is taken out and replaced by value of the last index: {any}\n", .{list.items});
+
+    try term_out.print("\n\nCapacity Operations:\n\n", .{});
+
+    try list.ensureTotalCapacity(20);
+    try term_out.print("After ensureTotalCapacity:\nitems={any}\ncapacity={}\n", .{ list.items, list.capacity });
+
+    list.shrinkAndFree(list.items.len);
+    try term_out.print("After shrinkAndFree:\nitems={any}\ncapacity={}\n", .{ list.items, list.capacity });
 }
